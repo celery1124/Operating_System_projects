4b2cf4b37553c8bc448fe710a3a6910e685ce106
celeryfake <morrisqin@gmail.com>
2018-02-21 21:59:23 -0600

    get rid of compilation error, type conversion basically, ready to test pagetable

--- MP3/page_table.C
+++ MP3/page_table.C
@@ -17,29 +17,29 @@ void PageTable::init_paging(ContFramePool * _kernel_mem_pool,
                             const unsigned long _shared_size)
 {
    kernel_mem_pool = _kernel_mem_pool;
-   process_mem_pool = _process_mem_pooll
+   process_mem_pool = _process_mem_pool;
    shared_size = _shared_size;
    Console::puts("Initialized Paging System\n");
 }
 
 PageTable::PageTable()
 {
-   unsigned long page_table;
+   unsigned long *page_table;
    current_page_table = this;
    // allocate and setup ptd
-   page_directory = kernel_mem_pool->get_frames(1);
+   page_directory = (unsigned long *)(kernel_mem_pool->get_frames(1) * PAGE_SIZE);
    // allocate pte
-   page_table = kernel_mem_pool->get_freams(1);
-   *page_directory = page_table<<12 + 0x03; // kernel mode, R/W, Present
-   for(int i=1;i<PT_ENTRIES_PER_PAGE;i++)
+   page_table = (unsigned long *)(kernel_mem_pool->get_frames(1) * PAGE_SIZE);
+   *page_directory = (unsigned long)page_table + 0x03; // kernel mode, R/W, Present
+   for(int i=1;i<ENTRIES_PER_PAGE;i++)
    {
-      *(page_directory+i) = i<<12 + 0x02; // kernel mode, R/W, not present
+      page_directory[i] = i<<12 + 0x02; // kernel mode, R/W, not present
    }
 
    // set up pte
-   for(int i=0;i<PT_ENTRIES_PER_PAGE;i++)
+   for(int i=0;i<ENTRIES_PER_PAGE;i++)
    {
-      *(page_table+i) = i<<12 + 0x03; // kernel mode, R/W, Present
+      page_table[i] = i<<12 + 0x03; // kernel mode, R/W, Present
    }
    Console::puts("Constructed Page Table object\n");
 }
@@ -47,13 +47,13 @@ PageTable::PageTable()
 
 void PageTable::load()
 {
-   write_cr3(page_directory);
+   write_cr3((unsigned long)page_directory);
    Console::puts("Loaded page table\n");
 }
 
 void PageTable::enable_paging()
 {
-   write_cr0(read_cr0() | 0x80000000);
+   write_cr0((unsigned long)(read_cr0() | 0x80000000));
    Console::puts("Enabled paging\n");
 }
 
