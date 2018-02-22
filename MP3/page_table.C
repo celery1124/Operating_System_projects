8bb18b5a0b55640212a4ee1e95c59f23270dccf7
celeryfake <morrisqin@gmail.com>
2018-02-21 21:40:40 -0600

    finish init_paging and constructoor of PateTable

--- MP3/page_table.C
+++ MP3/page_table.C
@@ -16,13 +16,28 @@ void PageTable::init_paging(ContFramePool * _kernel_mem_pool,
                             ContFramePool * _process_mem_pool,
                             const unsigned long _shared_size)
 {
-   assert(false);
+   kernel_mem_pool = _kernel_mem_pool;
+   process_mem_pool = _process_mem_pooll
+   shared_size = _shared_size;
    Console::puts("Initialized Paging System\n");
 }
 
 PageTable::PageTable()
 {
-   assert(false);
+   unsigned long page_table;
+   unsigned long prot_mask = 0x03; // kernel mode, R/W, Present
+   current_page_table = this;
+   // allocate and setup ptd
+   page_directory = kernel_mem_pool->get_frames(1);
+   // allocate pte
+   page_table = kernel_mem_pool->get_freams(1);
+   *page_directory = page_table<<12 + prot_mask;
+
+   // set up pte
+   for(int i=0;i<PT_ENTRIES_PER_PAGE;i++)
+   {
+      *(page_table+i) = i<<12 + prot_mask;
+   }
    Console::puts("Constructed Page Table object\n");
 }
 
