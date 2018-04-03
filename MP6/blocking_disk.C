94b76d9a1d3eff8563aff48d56c868f681b6554c
celeryfake <morrisqin@gmail.com>
2018-04-02 21:39:20 -0500

    uploading files for MP6

---
+++ MP6/blocking_disk.C
@@ -0,0 +1,48 @@
+/*
+     File        : blocking_disk.c
+
+     Author      : 
+     Modified    : 
+
+     Description : 
+
+*/
+
+/*--------------------------------------------------------------------------*/
+/* DEFINES */
+/*--------------------------------------------------------------------------*/
+
+    /* -- (none) -- */
+
+/*--------------------------------------------------------------------------*/
+/* INCLUDES */
+/*--------------------------------------------------------------------------*/
+
+#include "assert.H"
+#include "utils.H"
+#include "console.H"
+#include "blocking_disk.H"
+
+/*--------------------------------------------------------------------------*/
+/* CONSTRUCTOR */
+/*--------------------------------------------------------------------------*/
+
+BlockingDisk::BlockingDisk(DISK_ID _disk_id, unsigned int _size) 
+  : SimpleDisk(_disk_id, _size) {
+}
+
+/*--------------------------------------------------------------------------*/
+/* SIMPLE_DISK FUNCTIONS */
+/*--------------------------------------------------------------------------*/
+
+void BlockingDisk::read(unsigned long _block_no, unsigned char * _buf) {
+  // -- REPLACE THIS!!!
+  SimpleDisk::read(_block_no, _buf);
+
+}
+
+
+void BlockingDisk::write(unsigned long _block_no, unsigned char * _buf) {
+  // -- REPLACE THIS!!!
+  SimpleDisk::read(_block_no, _buf);
+}
