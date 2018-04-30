6e583309bb16ebb9759717bceab820f33eb937ca
celeryfake <morrisqin@gmail.com>
2018-04-30 15:34:56 -0500

    finish format part

--- MP7/file_system.C
+++ MP7/file_system.C
@@ -28,8 +28,7 @@
 /*--------------------------------------------------------------------------*/
 
 FileSystem::FileSystem() {
-    Console::puts("In file system constructor.\n");
-    assert(false);
+    disk = NULL;
 }
 
 /*--------------------------------------------------------------------------*/
@@ -37,13 +36,57 @@ FileSystem::FileSystem() {
 /*--------------------------------------------------------------------------*/
 
 bool FileSystem::Mount(SimpleDisk * _disk) {
-    Console::puts("mounting file system form disk\n");
-    assert(false);
+    if(disk != NULL)
+    {
+        Console::puts("Already mounted\n");
+        return false;
+    }
+    else
+    {
+        disk = _disk;
+        return true
+    }
 }
 
 bool FileSystem::Format(SimpleDisk * _disk, unsigned int _size) {
-    Console::puts("formatting disk\n");
-    assert(false);
+    // write buffer
+    unsigned char buf[512];
+    // 1, write the super block
+    *(unsigned int *)buf = _size;
+    _disk->write(0, buf);
+
+    // 2, wipe the file table;
+    for (int i=0;i<512;i++)
+        buf[i] = 0;
+    _disk->write(1, buf);
+
+    // 2, initialize inode bitmap
+    for (int i=0;i<64;i++)
+        buf[i] = 1;
+    _disk->write(2, buf);
+
+    // 3, initialize data block bitmap, need a little bit math
+    int data_block_num = _size / BLOCK_SIZE;
+    int db_bitmap_block_num;
+    if (data_block_num % (BLOCK_SIZE * 8) == 0)
+        db_bitmap_block_num = data_block_num / (BLOCK_SIZE * 8);
+    else
+        db_bitmap_block_num = data_block_num / (BLOCK_SIZE * 8) + 1;
+    int remain_bytes = data_block_num/8;
+    for (int i=0; i<db_bitmap_block_num; i++)
+    {
+        int bytes_in_block = BLOCK_SIZE;
+        if (i == db_bitmap_block_num - 1)
+            bytes_in_block = remain_bytes;
+        for(int j=0; j=bytes_in_block; j++)
+        {
+            buf[i] = 0xFF;
+        }
+        _disk->write(2, buf);
+        remain_bytes -= BLOCK_SIZE;
+    }
+
+    Console::puts("Format complete\n");    
 }
 
 File * FileSystem::LookupFile(int _file_id) {
